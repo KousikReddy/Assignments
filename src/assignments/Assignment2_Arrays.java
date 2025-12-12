14ba38a7446a63a49954040caa505763f3745a38
Kousik Reddy <Kousikreddy.95@gmail.com>
2025-12-12 09:01:53 +0530

    Assignment 2 - Arrays v2

--- src/assignments/Assignment2_Arrays.java
+++ src/assignments/Assignment2_Arrays.java
@@ -5,50 +5,32 @@ public class Assignment2_Arrays {
 	public static void main(String[] args) {
 		
 		String[][][] gradeSheet = {
-									{	//Sem-1(Subjects & Marks)
-										{"Mathematics 1","Pass(78)"},
-										{"Physics","Pass(85)"},
-										{"Chemistry","Fail(21)"},
-										{"Computer Programming","Pass(74)"},
-										{"Engineering Drawing","Pass(88)"},
-										{"Basic Electrical Engineering","Pass(79)"}
-									},
-									{   //Sem-2(Subjects & Marks)
-										{"Mathematics 2","Pass(82)"},
-										{"Mechanics","Pass(77)"},
-										{"Environmental Science","Pass(93)"},
-										{"Basic Electronics","Fail(19)"},
-										{"Engineering Physics","Fail(24)"},
-										{"Engineering Graphics","Pass(90)"}
-									},
-									{	//Sem-3(Subjects & Marks)
-										{"Data Structures","Pass(88)"},
-										{"Discrete Mathematics","Pass(81)"},
-										{"Digital Electronics","Pass(76)"},
-										{"Operating Systems","Fail(32)"},
-										{"Signals and Systems","Pass(85)"},
-										{"Object-Oriented Programming","Pass(78)"}
-									},
-									{	//Sem-4(Subjects & Marks)
-										{"Algorithms","Pass(91)"},
-										{"Computer Networks","Pass(73)"},
-										{"Database SYstems","Fail(19)"},
-										{"Microprocessors","Pass(80)"},
-										{"COmmunication Engineering","Pass(76)"},
-										{"Software Engineering","Pass(87)"}
-									},
-									{	//Sem-5(Subjects & Marks)
-										{"Probability and Stats","Pass(86)"},
-										{"Machine Learning","Pass(88)"},
-										{"Compiler Design","Pass(84)"},
-										{"Theory of Computation","Pass(95)"},
-										{"Embedded Systems","Pass(73)"},
-										{"Computer Graphics","Pass(90)"}
-									},
-									}; 
+				
+				{ // Sem 1
+                    {"Mathematics I", "Physics", "Chemistry", "Computer Programming", "Engineering Drawing", "Basic Electrical Engineering"},
+                    {"Pass(78)", "Pass(85)", "Fail(21)", "Pass(74)", "Pass(88)", "Pass(79)"}
+            },
+            { // Sem 2
+                    {"Mathematics II", "Mechanics", "Environmental Science", "Basic Electronics", "Engineering Physics", "Engineering Graphics"},
+                    {"Pass(82)", "Pass(77)", "Pass(93)", "Fail(19)", "Fail(24)", "Pass(90)"}
+            },
+            { // Sem 3
+                    {"Data Structures", "Discrete Mathematics", "Digital Electronics", "Operating Systems", "Signals and Systems", "Object-Oriented Programming"},
+                    {"Pass(88)", "Pass(81)", "Pass(76)", "Fail(32)", "Pass(85)", "Pass(78)"}
+            },
+            { // Sem 4
+                    {"Algorithms", "Computer Networks", "Database Systems", "Microprocessors", "Communication Engineering", "Software Engineering"},
+                    {"Pass(91)", "Pass(73)", "Fail(19)", "Pass(80)", "Pass(76)", "Pass(87)"}
+            },
+            { // Sem- 5
+                    {"Probability & Stats", "Machine Learning", "Compiler Design", "Theory of Computation", "Embedded Systems", "Computer Graphics"},
+                    {"Pass(86)", "Pass(88)", "Pass(84)", "Pass(95)", "Pass(73)", "Pass(90)"}
+            }
+    };
+				
 		
-		System.out.println("Semister-2 subject 4 and subject 5 names: "+gradeSheet[1][3][0]+", "+gradeSheet[1][4][0]);
-		System.out.println("Status/Marks of Semister-4 subject 3 and subject 6: "+gradeSheet[3][2][1]+", "+gradeSheet[3][5][1]);
+		System.out.println("Semister-2 subject 4 and subject 5 names: "+gradeSheet[1][0][3]+", "+gradeSheet[1][0][4]); //Basic Electronics & Engineering Physics
+		System.out.println("Status/Marks of Semister-4 subject 3 and subject 6: "+gradeSheet[3][1][2]+", "+gradeSheet[3][1][5]); //Fail(19) & Pass(87)
 		
 
 	}
